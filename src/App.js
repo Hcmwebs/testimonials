4a653518d43216bcdaa5759a129dd96eff362906
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-04-24 09:59:25 +1200

    finished the layout of the project
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.scss'
+import {FaChevronLeft,FaChevronRight} from 'react-icons/fa'
 
 const App = () => {
 	return (
@@ -6,28 +7,28 @@ const App = () => {
 			<h1 className='title'>Testimonials UI</h1>
 			<article className='card'>
 				<div className='card-body'>
-					<div className='section-content'>
+					<section className='section section-content'>
 						<div className='icon-logo'>Logo goes here</div>
 						<q>
 							Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
 							dignissimos culpa maxime iste deleniti ipsa.
 						</q>
-					</div>
-					<div className='section-loader'>
-						<span>...</span>
+					</section>
+					<section className='section section-loader'>
+						<span>....</span>
 						<div className='profile-info'>
 							<h4>Name goes here</h4>
 							<p>Job title goes here</p>
 						</div>
-					</div>
-				</div>
-				<div className='underline'></div>
-				<footer className='footer'>
+					</section>
+				<section className='section section-underline'></section>
+				<section className='section section-footer'>
 					<div className='btns'>
-						<button> chevron left </button>
-						<button> chevron right </button>
+						<button className='btn btn-left'> <FaChevronLeft/> </button>
+						<button className='btn btn-right'> <FaChevronRight/> </button>
 					</div>
-				</footer>
+				</section>
+				</div>
 			</article>
 		</main>
 	)
