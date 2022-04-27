63adb10e96b0430a7bc602e5c18aaf4bc70335cd
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-04-28 00:04:11 +1200

    -moved the title into the testimonial component -added a reviews container div
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -5,39 +5,42 @@ import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
 const Testimonial = ({ reviews }) => {
 	return (
 		<>
-			{reviews.map((review) => {
-				const { id, avatar, name, message, designation } = review
-				return (
-					<article key={id} className='card'>
-						<div className='card-body'>
-							<section className='section section-content'>
-								<div className='icon-logo'>
-									<img src={avatar} alt={name} className='img' />
-								</div>
-								<q>{message}</q>
-							</section>
-							<section className='section section-loader'>
-								<span>....</span>
-								<div className='profile-info'>
-									<h4>{name}</h4>
-									<p>{designation}</p>
-								</div>
-							</section>
-							<section className='section section-underline'></section>
-							<section className='section section-footer'>
-								<div className='btns'>
-									<button className='btn btn-left'>
-										<FaChevronLeft />
-									</button>
-									<button className='btn btn-right'>
-										<FaChevronRight />
-									</button>
-								</div>
-							</section>
-						</div>
-					</article>
-				)
-			})}
+			<h1 className='title'>Testimonials UI</h1>
+			<div className='reviews'>
+				{reviews.map((review) => {
+					const { id, avatar, name, message, designation } = review
+					return (
+						<article key={id} className='card'>
+							<div className='card-body'>
+								<section className='section section-content'>
+									<div className='icon-logo'>
+										<img src={avatar} alt={name} className='img' />
+									</div>
+									<q>{message}</q>
+								</section>
+								<section className='section section-loader'>
+									<span>....</span>
+									<div className='profile-info'>
+										<h4>{name}</h4>
+										<p>{designation}</p>
+									</div>
+								</section>
+								<section className='section section-underline'></section>
+								<section className='section section-footer'>
+									<div className='btns'>
+										<button className='btn btn-left'>
+											<FaChevronLeft />
+										</button>
+										<button className='btn btn-right'>
+											<FaChevronRight />
+										</button>
+									</div>
+								</section>
+							</div>
+						</article>
+					)
+				})}
+			</div>
 		</>
 	)
 }
