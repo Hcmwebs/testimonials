d84a79e81f431180fe92530e74a2e094f47a8357
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-04-28 09:14:02 +1200

    started the functionality for the slider
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -1,14 +1,16 @@
 import './index.scss'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
 
 const Testimonial = ({ reviews }) => {
+
 	return (
 		<>
 			<h1 className='title'>Testimonials UI</h1>
 			<div className='reviews'>
-				{reviews.map((review) => {
-					const { id, avatar, name, message, designation } = review
+				{reviews.map((review, index) => {
+					const { id, avatar, name, message, designation } = review;
+					
 					return (
 						<article key={id} className='card'>
 							<div className='card-body'>
